c9daa52f0e8c8d3f25e5a3eca7470da97f300136
Mateusz <mat.bereda@gmail.com>
2025-03-18 16:32:08 +0100

    update

---
+++ src/App4.java
@@ -0,0 +1,35 @@
+public class App4 {
+    public static void main(String[] args) {
+        int x;
+
+        if(true) {
+            //int x;
+            int y;
+        }
+
+        for (int i = 0; i < 10; i++) {
+
+            int z = 0;
+
+            for (int j = 0; j < 10; j++) {
+                z = z + i * j;
+            }
+
+            System.out.println(z);
+        }
+
+        int y;
+        //System.out.println(y);
+    }
+
+    public static void cos() {
+        System.out.println("funkcja !!!");
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+    }
+}
